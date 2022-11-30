adc0c1900798d7f5e3481275d73cd0d19cc4680d
Ekaterina Kloss <kloss-kate@mail.ru>
2022-11-30 14:23:28 +0300

    Task 1 completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Seminar 1. Homework Kloss E.
+//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 > num2)
+{
+    Console.WriteLine("Max: " + num1);
+    Console.WriteLine("Min: " + num2);
+}
+else
+{
+    Console.WriteLine("Max: " + num2);
+    Console.WriteLine("Min: " + num1);
+}
+*/
