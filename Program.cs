6288dec4451f0c39d27ddd49bfabfbe99802ca15
Ekaterina Kloss <kloss-kate@mail.ru>
2022-11-30 15:09:31 +0300

    Task 2 completed

--- Program.cs
+++ Program.cs
@@ -16,3 +16,33 @@ else
     Console.WriteLine("Min: " + num1);
 }
 */
+//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+  if (num1 > num3)
+  {
+    Console.WriteLine("Max: " + num1);
+  }
+  else 
+    {
+    Console.WriteLine("Max: " + num3);
+    }
+} 
+else
+{
+    if (num2 > num3)
+    {
+        Console.WriteLine("Max: " + num2);
+    }
+    else
+    {
+        Console.WriteLine("Max: " + num3);
+    }
+}
